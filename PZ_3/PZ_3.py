6a86f2ee9d0a93c56a0cf924be9c660d069057db
DanilRumyantsev <danilrumancev48@gmail.com>
2023-11-11 19:40:16 +0300

    last3_commit

--- PZ_3/PZ_3.py
+++ PZ_3/PZ_3.py
@@ -1,13 +1,8 @@
 #Проверить условие "Число читается одинаково, слева на право и справа налево
 
-def function(number): #Создаём функцию
-    number_str = str(number) #Переводим числа в строку, чтобы потом её перевернуть
+your_number = input("Введите четырёхзначное число: ")
 
-    return number_str == number_str[::-1] #возвращаем перевернутое число
-
-your_number = input("Введите четырёхзначное число")
-
-if function(your_number): #Создаем условие
+if your_number == your_number[::-1]:
     print(f"{your_number} - число читается одинаково")
 else:
     print(f"{your_number} - число не читается одинаково")
